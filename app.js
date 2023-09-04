a51021c9ef5c3cd48247d16b0c8d970db4dc3228
AGmaj <adrian.gmaj@gmail.com>
2023-09-04 15:14:59 +0200

    testimonials

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 // // Grabbing the target elements from the HTML File
 const list = document.querySelector(".gallery-carousel__img-container--list");
 const imgs = Array.from(list.children);
@@ -8,17 +7,12 @@ const prevButton = document.querySelector(".gallery-carousel__btn--left");
 const carouselNav = document.querySelector(".gallery-carousel__nav");
 const dots = Array.from(carouselNav.children);
 
-console.log(list);
-console.log(imgs);
-// // console.log(nextButton);
-// // console.log(prevButton);  
-// // console.log(carouselNav);
 
 // // Getting the width of our images
-const imgWidth = imgs[0].getBoundingClientRect();
-// const imgWidth = imgs[0].getBoundingClientRect().height;
-// const imgWidth = imgs[0].getBoundingClientRect().width;
-// console.log(imgWidth);
+// // const imgWidth = imgs[0].getBoundingClientRect();
+// // const imgWidth = imgs[0].getBoundingClientRect().height;
+const imgWidth = imgs[0].getBoundingClientRect().width;
+// // console.log(imgWidth);
 
 // // Arranging the images next to one another
 // // function setImgPosition(img, index) {
